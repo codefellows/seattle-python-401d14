b0e620d01721495637cd66da8353a1440a369264
JB-Tellez <jb.tellez@gmail.com>
2020-06-18 14:30:37 -0700

    adds tests for demo 29

--- class-29/demo/custom-user/users/models.py
+++ class-29/demo/custom-user/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.username
